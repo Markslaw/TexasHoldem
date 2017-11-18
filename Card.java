baf413f638b51e7e153109c2f85df366517ce001
Mark <shaunalily9@gmail.com>
2017-11-18 14:48:47 +0000

    test

--- Card.java
+++ Card.java
@@ -8,5 +8,26 @@ public class Card {
     private int suit;
     private int value;
 
+    //getters
+    public int getSuit(){
+        return suit;
+    }
+
+    public int getValue(){
+        return value
+    }
+
+    //setters
+    public void setSuit(int suit){
+        this.suit = suit;
+    }
+
+    public void setValue(int value){
+        this.value = value;
+    }
+
+    //2 arguement constructor
+    public Card
+
 
 }
